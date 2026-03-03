970aac10c6fb182e142ed21cec43aaa90662881c
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-03-03 16:52:59 -0600

    Create provider.tf
    Sample provider.tf for bitbucket repo

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    bitbucket = {
+      source  = "DrFaust92/bitbucket"
+      version = "~> 2.40.1"
+    }
+  }
+}
+
+provider "bitbucket" {
+  username  = "terraformuser@socpuppets.com"
+  # you would set the API TOKEN under  the APP and scope # call this a s variable
+  password  = "ATATT3xFfGF0uALH2X6wrCx6Wr16VUubNFCXVSSbXwH9UABMIeVThj74jVffSW6Rj52C69o_0jUk_JloRT8bzesH9wrPdjgseQ93y61e5Qyv8Twlksd5GV55zVxfpNsxQKMV2B1ltmt3c5iSfol_E6i-YJrqbRkupwcw26xik01pPtcCVEaYz4SA=0FB47729"
+
+}
