e057644782f6bd0cf08431836084c22b50e7a6b7
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-03-03 16:55:11 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "bitbucket_repository" "kentest" {
+  owner       = var.workspace
+  name        = "kentest"
+  description = "kentest repository managed by Terraform example with no prevention to destroy"
+
+  is_private  = true
+  scm         = "git"
+  has_issues  = true
+  has_wiki    = false
+  fork_policy = "no_public_forks"
+
+  lifecycle {
+    #prevent_destroy = true
+  }
+}
+
+
+# repo should be protected from terraform destroy, this allows you ensure you do not destroy a report by accident 
+
+resource "bitbucket_repository" "kentest2" {
+  owner       = var.workspace
+  name        = "kentest2"
+  description = "kentest repository managed by Terraform example with prevention to destroy"
+
+  is_private  = true
+  scm         = "git"
+  has_issues  = true
+  has_wiki    = false
+  fork_policy = "no_public_forks"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
