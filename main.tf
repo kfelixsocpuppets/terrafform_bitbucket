663e720ed2ed33cab00fabe7599830a984c1e645
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-03-03 17:04:35 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "bitbucket_repository" "kentest" {
+resource "bitbucket_repository" "kentest_example1" {
   owner       = var.workspace
-  name        = "kentest"
+  name        = "kentest_example1"
   description = "kentest repository managed by Terraform example with no prevention to destroy"
 
   is_private  = true
@@ -17,7 +17,7 @@ resource "bitbucket_repository" "kentest" {
 
 # repo should be protected from terraform destroy, this allows you ensure you do not destroy a report by accident 
 
-resource "bitbucket_repository" "kentest2" {
+resource "bitbucket_repository" "kentest2_example" {
   owner       = var.workspace
   name        = "kentest2"
   description = "kentest repository managed by Terraform example with prevention to destroy"
