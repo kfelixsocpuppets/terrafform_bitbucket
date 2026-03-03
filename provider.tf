014a57f21d3a3e7c3387861cf31646aee3e6e21d
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-03-03 17:05:20 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,9 @@ terraform {
 
 provider "bitbucket" {
   username  = "terraformuser@socpuppets.com"
-  # you would set the API TOKEN under  the APP and scope # call this a s variable
+  #
+  # you would set the API TOKEN under  the APP and scope # call this a s variable var.password
+  #
   password  = "ATATT3xFfGF0uALH2X6wrCx6Wr16VUubNFCXVSSbXwH9UABMIeVThj74jVffSW6Rj52C69o_0jUk_JloRT8bzesH9wrPdjgseQ93y61e5Qyv8Twlksd5GV55zVxfpNsxQKMV2B1ltmt3c5iSfol_E6i-YJrqbRkupwcw26xik01pPtcCVEaYz4SA=0FB47729"
 
 }
